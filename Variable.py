40327225ee68066bec3c39093f307fa3df252ad3
nikhilp53813 <nikhilp53813@gmail.com>
2025-07-10 13:43:21 +0530

    update

--- Variable.py
+++ Variable.py
@@ -62,3 +62,32 @@ print(x,y,z)
 #product_name-->Snake case (When using variables and functions)
 
 
+product_brand= "Hyundai creta"
+product_rating= 4.6
+product_price=11,60,000
+seating_capacity=5
+Fuel_tank_capacity="50litres"
+no_of_cylinders=4
+Ground_clearance_unladen="190mm"
+transmission_type="automatic"
+body_type="suv"
+engine_displacement="1493cc"
+mileage="19.1kmpl"
+max_power="114bhp@4000rpm"
+Fuel_type="Diesel"
+max_torque="250Nm@1500-2750rpm"
+air_bag_present=True 
+
+print(product_brand)
+print(product_rating)
+print(seating_capacity)
+print(Fuel_tank_capacity)
+print(no_of_cylinders)
+print(transmission_type)
+print(body_type)
+print(engine_displacement)
+print(mileage)
+print(max_power)
+print(Fuel_type)
+print(max_torque)
+print(air_bag_present)
