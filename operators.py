a4f943cf71f940573fd436f08706f1b5d7688ff8
nikhilp53813 <nikhilp53813@gmail.com>
2025-07-11 09:23:41 +0530

    update

---
+++ operators.py
@@ -0,0 +1,25 @@
+# Interpolation
+name = "mahesh"
+session = 8
+(f"my name is {name} session is at {session}")
+
+#Operators
+
+#Arithmetic operators
+num1 = 3
+num2 = 2
+print(num1+num2)
+print(num1-num2)
+print(num1*num2)
+print(num1/num2) #quotient
+print(num1%num2) # remainder
+print(num1//num2) # quotient is roundoff to the nearest integer
+print(num1**num2) # expo (square of)
+
+#Compound assignment operators
+num=10
+#num*=5
+num+=5
+print(num)
+
+#comparison operators
